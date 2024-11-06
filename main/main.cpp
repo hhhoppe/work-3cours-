45c1112efbf7b64d7612250ecbeef42a9742deb3
hhhoppe <sergeydergynov038.uniads@gmail.com>
2024-11-07 01:36:30 +0300

    added realization TPair class, test and main() implementation to demonstrate the operation of this class

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,6 @@
-// Copyright 2024 Dergynov Sergey
+﻿// Copyright 2024 Dergynov Sergey
 
-#define EASY_EXAMPLE
+#define Pair
 
 #ifdef EASY_EXAMPLE
 
@@ -45,7 +45,44 @@ int main() {
 
 int main() {
 
-    ///
+    // Создение пары с помощью конструктора по умолчанию
+    TPair<int, int> pair1;
+    std::cout << "Defolt constructor pair: ("
+        << pair1.first() << ", " << pair1.second() << ")" << std::endl;
+
+    // Создение пары с помощью конструктора с параметром
+    TPair<int, int> pair2(12, 2);
+    std::cout << "Parametr constructor pair: ("
+        << pair2.first() << ", " << pair2.second() << ")" << std::endl;
+
+    // Работа сеттеров и геттеров
+    pair1.set_first(10);
+    pair1.set_second(20);
+    std::cout << "Using setters on pair1: ("
+        << pair1.first() << ", " << pair1.second() << ")" << std::endl;
+
+    // Работа конструктора копирования
+    TPair<int, int> pair3(pair2);
+    std::cout << "Copy constructor pair3 from pair2: ("
+        << pair3.first() << ", " << pair3.second() << ")" << std::endl;
+
+    // Работа оператора присваивания
+    pair3 = pair1;
+    std::cout << "Assigning pair1 from pair3: ("
+        << pair3.first() << ", " << pair3.second() << ")" << std::endl;
+
+    // Работа оператора сравнения
+    if (pair1 == pair3) {
+        std::cout << "pair1 and pair3 ==" << std::endl;
+    } else {
+        std::cout << "pair1 and pair3 !=" << std::endl;
+    }
+
+    if (pair1 == pair2) {
+        std::cout << "pair1 and pair2 ==" << std::endl;
+    } else {
+        std::cout << "pair1 and pair2 !=" << std::endl;
+    }
 
     return 0;
 }
--- tests/test_pair.cpp
+++ tests/test_pair.cpp
@@ -1,4 +1,60 @@
-// Copyright 2024 Dergynov Sergey
+﻿// Copyright 2024 Dergynov Sergey
 
 #include <gtest.h>
 #include "../lib_pair/pair.h"
+
+// Проверка конструктора по умолчанию
+TEST(TestTPairLib, defoult) {
+	TPair<int, double> pair;
+	EXPECT_EQ(pair.first(), 0);
+	EXPECT_EQ(pair.second(), 0.0);
+}
+
+// Проверка конструктора с параметрами
+TEST(TestTPairLib, parametr) {
+	TPair<int, char> pair(11, 'a');
+	EXPECT_EQ(pair.first(), 12);
+	EXPECT_EQ(pair.second(), 4.5);
+}
+
+// Проверка конструктора копирования
+TEST(TestTPairLib, copy) {
+	TPair<double, char> original(14.5, 'z');
+	TPair<double, char> copy(original);
+	EXPECT_EQ(copy.first(), 14.5);
+	EXPECT_EQ(copy.second(), 'z');
+}
+
+// Проверка геттеров
+TEST(TestTPairLib, getters) {
+	TPair<int, int> pair(11, 98);
+	EXPECT_EQ(pair.first(), 11);
+	EXPECT_EQ(pair.second(), 98);
+}
+
+// Проверка сеттеров
+TEST(TestTPairLib, setters) {
+	TPair<double, double> pair;
+	pair.set_first(11.1);
+	pair.set_second(22.2);
+	EXPECT_EQ(pair.first(), 11.1);
+	EXPECT_EQ(pair.second(), 22.2);
+}
+
+// Проверка оператора присваивания
+TEST(TestTPairLib, assignment) {
+	TPair<char, char> pair1('a', 'b');
+	TPair<char, char> pair2;
+	pair1 = pair2;
+	EXPECT_EQ(pair2.first(), 'a');
+	EXPECT_EQ(pair2.second(), 'b');
+}
+
+// Проверка оператора сравнения
+TEST(TestTPairLib, comparison) {
+	TPair<int, int> pair1(34, 12);
+	TPair<int, int> pair2(34, 12);
+	TPair<int, int> pair3(38, 11);
+	EXPECT_TRUE(pair1 == pair2);
+	EXPECT_FALSE(pair1 == pair3);
+}
